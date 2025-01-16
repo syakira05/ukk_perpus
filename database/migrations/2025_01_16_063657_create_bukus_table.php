acc49d5f7a52e85b3928a5412db40be8a543bd77
syakira05 <sy4kk05@gmail.com>
2025-01-16 15:05:42 +0700

    initial commit

---
+++ database/migrations/2025_01_16_063657_create_bukus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kategori_id')->constrained('kategori_buku');
+            $table->string('judul');
+            $table->string('pengarang');
+            $table->string('penerbit');
+            $table->string('tahun_terbit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+};
